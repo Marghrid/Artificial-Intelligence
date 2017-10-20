782ee0d778d0a70c7ce33877b0924d8e52793e5a
Marghrid <marghrid@hotmail.com>
2017-10-20 19:33:29 +0100

    Renamed sg_g3.py and it passes every test on mooshack.

--- tests.py
+++ tests.py
@@ -10,7 +10,7 @@ tests = [
 		[[1,1,3,3,3,3],[2,2,3,3,3,3],[2,2,3,3,3,3],[2,2,3,3,3,3],[2,2,3,3,3,3]],
 		[[3,3,0,3,3,3],[2,2,0,3,3,3],[2,2,0,3,3,3],[2,2,1,3,3,3],[2,2,1,3,3,3]],
 		[[1,3,2,1,2,1,2,2,1,2,2,1,1,3,1],[1,3,3,2,1,2,2,2,3,1,2,1,2,3,1],[1,1,1,2,3,2,3,3,2,2,3,1,1,3,1],[1,2,2,2,3,3,3,3,1,2,1,2,1,3,2],[1,3,1,3,2,2,2,2,3,1,1,2,3,2,1],[1,1,2,2,2,1,1,3,2,1,2,3,1,3,1],[3,1,3,2,2,2,3,3,3,1,3,3,2,1,1],[3,2,1,2,1,3,1,2,1,2,3,1,1,3,3],[2,3,1,2,3,3,1,2,3,3,3,2,1,1,1],[2,2,1,1,2,1,2,2,1,1,3,2,2,2,2]],
-
+		[[4,4,4,2,3,2,2,2,2,1,2,3,4,4,3],[1,4,4,2,1,2,4,1,1,2,4,4,3,4,1],[2,3,2,2,2,4,3,2,2,2,2,1,4,2,3],[3,3,3,4,2,2,4,3,3,4,4,2,4,1,1],[1,3,3,4,3,1,1,1,1,1,1,2,4,2,3],[4,2,3,3,1,3,3,2,2,4,3,3,2,1,4],[3,1,2,1,2,3,2,4,2,4,4,3,4,4,2],[3,4,3,1,1,3,4,4,1,2,3,1,1,4,2],[1,3,1,3,2,1,2,1,3,4,4,3,2,3,1],[1,2,1,2,2,2,1,3,2,4,3,2,1,2,1]]
 ];
 
 def greedy_search(problem, h=None):
@@ -29,16 +29,17 @@ for test in tests:
 #	print_board(test)
 	print()
 
-	print("DFS:")
-	problem = InstrumentedProblem(same_game(test))
-	initial_time = perf_counter()
-	depth_first_tree_search(problem)
-	final_time = perf_counter() - initial_time
-	print("Tempo: ", end="")
-	print(final_time)
-	print("Nós: ", end = "")
-	print(problem)
-	print()
+	if i < 10:
+		print("DFS:")
+		problem = InstrumentedProblem(same_game(test))
+		initial_time = perf_counter()
+		depth_first_tree_search(problem)
+		final_time = perf_counter() - initial_time
+		print("Tempo: ", end="")
+		print(final_time)
+		print("Nós: ", end = "")
+		print(problem)
+		print()
 
 	print("Greedy search:")
 	problem = InstrumentedProblem(same_game(test))
